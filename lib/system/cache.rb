5e73bdc9afe61964ae6601a11686c34ddf8ea121
Viktor <viktor.tassi@oneminorder.com>
2018-03-13 15:30:37 +0100

    -

--- lib/system/cache.rb
+++ lib/system/cache.rb
@@ -1,21 +1,23 @@
 require 'connection_pool'
 
 module System
+  
+  #Full static class may provide low mem usage, and can simply share pool across threads. Redis-ing is a thread safe
   class Cache
 
     def self.hset(category, key, value, validity_key, ttl = nil)
       self.pool.with  do |conn|
-        conn.hmset("#{category}:#{key}", "value", ( (value.is_a? String) ? value : value.to_json ), "validity_key", validity_key)
-        conn.expireat( "#{category}:#{key}", (Time.zone.now.to_i+(ttl.present? ? ttl : self.ttl)).to_i)
+        conn.hmset(build_key(category, key), "value", ( (value.is_a? String) ? value : value.to_json ), "validity_key", validity_key)
+        conn.expireat( build_key(category, key), (Time.zone.now.to_i+(ttl.present? ? ttl : self.ttl)).to_i)
       end
     end
 
     def self.hget(category, key, validity_hash, default_value = nil)
       self.pool.with  do |conn|
-        if (conn.hget "#{category}:#{key}" "validity_hash") == validity_hash
+        if  conn.hget(build_key(category, key),"validity_hash") == validity_hash
           #retouch
-          conn.expireat("#{category}:#{key}",(Time.zone.now+self.ttl).to_i)
-          return conn.hget "#{category}:#{key}" "value"
+          conn.expireat(build_key(category, key),(Time.zone.now+self.ttl).to_i)
+          return conn.hget(build_key(category, key), "value")
         end
       end
       default_value
@@ -23,31 +25,31 @@ module System
 
     def self.hgetall(category, key, default_value = nil)
       self.pool.with  do |conn|
-        conn.exists("#{category}:#{key}") ? conn.hgetall("#{category}:#{key}") : default_value
+        conn.exists(build_key(category, key)) ? conn.hgetall(build_key(category, key)) : default_value
       end
     end
 
     def self.hsetall(category, key, value, ttl = nil)
       self.pool.with  do |conn|
-        conn.mapped_hmset("#{category}:#{key}", value) && conn.expireat( "#{category}:#{key}", (Time.zone.now.to_i+(ttl.present? ? ttl : self.ttl)).to_i)
+        conn.mapped_hmset(build_key(category, key), value) && conn.expireat( build_key(category, key), (Time.zone.now.to_i+(ttl.present? ? ttl : self.ttl)).to_i)
       end
     end
 
     def self.set(category, key, value, ttl = nil)
       self.pool.with  do |conn|
-        conn.set("#{category}:#{key}", (value.is_a? String) ? value : value.to_json) && conn.expireat( "#{category}:#{key}", (Time.zone.now.to_i+(ttl.present? ? ttl : self.ttl)).to_i)
+        conn.set(build_key(category, key), (value.is_a? String) ? value : value.to_json) && conn.expireat( build_key(category, key), (Time.zone.now.to_i+(ttl.present? ? ttl : self.ttl)).to_i)
       end
     end
 
     def self.get(category, key, default_value = nil)
       self.pool.with  do |conn|
-        conn.exists("#{category}:#{key}") ? conn.get("#{category}:#{key}") : default_value
+        conn.exists(build_key(category, key)) ? conn.get(build_key(category, key)) : default_value
       end
     end
 
     def self.exists(category, key)
       self.pool.with  do |conn|
-        conn.exists("#{category}:#{key}")
+        conn.exists(build_key(category, key))
       end
     end
 
@@ -71,5 +73,10 @@ module System
     cattr_accessor :redis_pool
     cattr_accessor :database
     cattr_accessor :ttl
+
+    def self.build_key(category, key)
+      "#{category}:#{key}"
+    end
+
   end
 end
