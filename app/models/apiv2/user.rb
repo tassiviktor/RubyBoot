b9015cb2aa487d1ebe98cb87f66e071772398732
Viktor <viktor.tassi@oneminorder.com>
2018-05-04 16:38:49 +0200

    refactorings

--- app/models/apiv2/user.rb
+++ app/models/apiv2/user.rb
@@ -5,13 +5,10 @@ module Apiv2
   # Users
   class User < ApplicationRecord
     belongs_to :realm
-    
+
     #fetching at compile time
     BELONGS_TO = reflect_on_all_associations(:belongs_to).map(&:plural_name)
     HAS_MANY = reflect_on_all_associations(:has_many).map(&:plural_name)
 
-    def to_json(options = {})
-      {id: self.id}
-    end
   end
 end
